d76824135a3c19a76d393f3bc31622dd626ff146
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-10-15 17:17:56 +0530

    Time: 7 ms (96.28%), Space: 19.4 MB (97.39%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto &x:nums){
-            if(s.find(x)==s.end())s.insert(x);
-            else s.erase(x);
-        }
-        for(auto &x:s) return x;
-        return -1;
+        int ans=0;for(auto &x:nums)ans^=x;return ans;
     }
 };

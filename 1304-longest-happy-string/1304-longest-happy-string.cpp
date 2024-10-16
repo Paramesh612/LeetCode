0b417c628f21f98d79de5872fce14d50fe01cc1c
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-10-16 20:45:08 +0530

    Time: 3 ms (43.32%), Space: 7.8 MB (30.3%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -1,65 +1,38 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        string ans="";
-
-        int counta,countb,countc;
-        counta=countb=countc=0;
-
+        string ans = "";
+        int counta, countb, countc;
+        counta = countb = countc = 0;
         int count[3];
-        priority_queue<pair<int,char>> pq;
-        
-        pq.push({a,'a'});
-        pq.push({b,'b'});
-        pq.push({c,'c'});
-
-        while(a>0 || b>0 ||c>0){
-
-            vector<pair<int,char>> temp;
-            // temp.push_back(current);
-
-            while(!pq.empty() && !(pq.top().first>0 && count[pq.top().second-'a']<2)){
-                auto toPushIn = pq.top();
+        priority_queue<pair<int, char>> pq;
+        pq.push({a, 'a'});
+        pq.push({b, 'b'});
+        pq.push({c, 'c'});
+        while (a > 0 || b > 0 || c > 0) {
+            vector<pair<int, char>> temp;
+            while (!pq.empty() &&
+                   !(pq.top().first > 0 && count[pq.top().second - 'a'] < 2)) {
+                auto& toPushIn = pq.top();
                 temp.push_back(toPushIn);
                 pq.pop();
             }
-
-            if(pq.empty())break;
-
-            pair<int,char> current = pq.top();
+            if (pq.empty())
+                break;
+            pair<int, char> current = pq.top();
             pq.pop();
-            ans+=current.second;
+            ans += current.second;
             current.first--;
-            count[current.second-'a']++;
-            
-            for(int i=0;i<3;i++){
-                if(i!=(current.second-'a')) count[i]=0;
+            count[current.second - 'a']++;
+            for (int i = 0; i < 3; i++) {
+                if (i != (current.second - 'a'))
+                    count[i] = 0;
             }
-            if(current.first>0) pq.push(current);
-
-            for(auto &x:temp){
+            if (current.first > 0)
+                pq.push(current);
+            for (auto& x : temp) {
                 pq.push(x);
             }
-            
-
-            /*
-            if(a>0 && counta<=2){
-                ans+='a';
-                counta++;
-                a--;
-                countb=countc=0;
-            }else if(b>0 && countb<=2){
-                ans+='b';
-                countb++;
-                b--;
-                counta=countc=0;
-            }else if(c>0 && countc<=2){
-                ans+='c';
-                c--;
-                countc++;
-                counta = countb=0;
-            }else break;
-            */
         }
         return ans;
     }

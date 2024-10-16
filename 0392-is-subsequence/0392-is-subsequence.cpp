3ef8ed540b2f09db0c8457430250edd775d50840
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-10-16 09:22:24 +0530

    Time: 5 ms (11.37%), Space: 9.4 MB (5.2%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int m = s.size() , n= t.size();
-        vector<vector<int>>dp(m+1 ,vector<int>(n+1,0)); //INT_MAX-100
+        int m = s.size(), n = t.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        for(int i=1;i<=m;i++){
-            // dp[i][0]=0;
-            // dp[0][j]=0;
-            for(int j=1;j<=n;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        // cout<<"The LCS: "<<dp[m][n];
-        return dp[m][n]>=m;
+
+        return dp[m][n] >= m;
     }
 };

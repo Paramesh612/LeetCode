0e63ee0b76e1a56d790e9e5e70b9e935987334de
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-10-16 13:45:30 +0530

    Time: 22 ms (22.36%), Space: 11.6 MB (13.82%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+        for(char x:magazine){
+            mp[x]++;
+        }
+        for(char x:ransomNote){
+            mp[x]--;
+            if(mp[x]<0) return false;
+        }
+        return true;
+    }
+};

f79352a1b0d47bc06a526cd078b3f2c615b88b51
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-10-21 22:31:08 +0530

    Time: 0 ms (100%), Space: 9.4 MB (20.24%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int> mp;
+        for( auto &x: t){
+            mp[x]++;
+        }
+
+        for(auto &x:s){
+            mp[x]--;
+            if(mp[x]==0)mp.erase(x);
+        }
+
+        for(auto &x:mp){
+            return x.first;
+        }
+        return '#';
+    }
+};

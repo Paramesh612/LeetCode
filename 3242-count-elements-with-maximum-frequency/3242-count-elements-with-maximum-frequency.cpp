e1056f3845a2e982cae7e9b946f9e6393a35f494
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-10-31 18:17:17 +0530

    Time: 4 ms (16.38%), Space: 23.7 MB (10.55%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -5,10 +5,11 @@ public:
         int maxx=INT_MIN ,count=0;
         for(auto &x:nums){
             mp[x]++;
-            maxx=max(maxx,mp[x]);
+            if(mp[x]>maxx){
+                count=1;
+                maxx=mp[x];
+            }else if(maxx==mp[x]) count++;
         }
-
-        for(auto &x:mp)if(maxx==x.second)count++;
         return count*maxx;
     }
 };

cd3e9202d633b37b5824c479a472206717b4c6fb
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-11-04 21:03:57 +0530

    Time: 0 ms (100%), Space: 9.6 MB (11.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        else if(n==2) return max(nums[0],nums[1]);
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
         
-        vector<int> dp(n,0);
+        vector<int> dp(nums.size(),0);
         dp[0]=nums[0];
-        dp[1]=nums[1];
-        dp[2]=nums[0]+nums[2];
-        cout<<dp[2];
-        // int ans=max(dp[0],max(dp[1],dp[2]));
-
-        for(int i=3 ;i<n;i++){
-            dp[i]= nums[i]+max(dp[i-2],dp[i-3]);
+        for(int i=1;i<nums.size();i++){
+            dp[i]=max(nums[i],dp[i-1]);
+            if(i>1) dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
         }
-        return max(dp[n-1],dp[n-2]);
-
-        // int prev=nums[1];
-        // int prevvv=nums[0];
-        // int ans=INT_MIN;
-        // for(int i=2;i<n;i++){
-        //     ans=(nums[i]+max(prev,prevvv));
-        //     prevvv=prev;
-        //     prev=ans;
-        // }
-        // return ans;
+        return dp[nums.size()-1];
     }
 };

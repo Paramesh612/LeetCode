03e0ac3cb74957a15094d4a7936980a42a49f812
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-11-16 16:03:31 +0530

    Time: 0 ms (100%), Space: 7.3 MB (83.33%) - LeetHub

---
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+    int brokenCalc(int startValue, int target) {
+        int count=0;
+        while(startValue<target){
+            if(target&1) target++;
+            else target/=2;
+            count++;
+        }
+        return count+(startValue-target);
+    }
+};

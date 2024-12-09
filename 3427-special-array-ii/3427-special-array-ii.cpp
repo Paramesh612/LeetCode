56acf62614f2776d0493d417eb3f3d23e33ab0e4
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-12-09 12:07:59 +0530

    Time: 3 ms (92.97%), Space: 126.5 MB (66.56%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int>prefix(nums.size());
-        prefix[0]=0;
-
-        for(int i=1;i<nums.size();i++){
-            prefix[i]=prefix[i-1]+( ((nums[i]&1)^(nums[i-1]&1)) ? 0:1 );
-        }
-
         vector<bool> ans;
-        for(auto &x:queries){
-            if( prefix[x[0]] == prefix[x[1]] ) ans.push_back(true);
-            else ans.push_back(false);
-        }
-
+        prefix[0]=0;
+        for(int i=1;i<nums.size();i++)prefix[i]=prefix[i-1]+( ((nums[i]&1)^(nums[i-1]&1)) ? 0:1 );
+        for(auto &x:queries) prefix[x[0]] == prefix[x[1]]?ans.push_back(true):ans.push_back(false);
         return ans;
     }
 };

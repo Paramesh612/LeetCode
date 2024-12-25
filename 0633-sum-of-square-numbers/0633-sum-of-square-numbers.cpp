bb3dddb1777406eb0848d5f15c1c25f06db058e4
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-12-25 20:22:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (24.23%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -11,9 +11,9 @@ public:
     // }
 
     bool judgeSquareSum(int c) {
-        long long low = 0 , high = sqrt(c);
+        long long val, low = 0 , high = sqrt(c);
         while(low <= high){
-            long long val = low*low + high*high;
+            val = low*low + high*high;
             if(val  == c) return true;
             else if(val > c) high--;
             else low++;

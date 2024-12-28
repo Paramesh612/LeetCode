d736a13f7ae3ade9b1d1d407dea3d804169719e5
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-12-29 00:10:43 +0530

    Time: 3 ms (68.99%), Space: 15.6 MB (47.52%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    // vector<string> mult(vector<string> a, vector<string> b ){
+    //     vector<string> ret;
+    //     for(auto &x:a){
+    //         for(auto &y:b){
+    //             ret.push_back(x+y);
+    //         }
+    //     }
+    //     return ret;
+    // }
+
+    template <typename T>
+    void printt(vector<T> a){
+        for(auto x:a){
+            cout<<x<<", ";
+        }
+        cout<<endl;
+    }
+public:
+    // See first 5 mins or so of NeetCode's vid
+
+    // Maximum that can be opened or closed = n; so 
+    void createCombos( int open_avail , int close_avai, string temp , vector<string> &ans){ //,vector<string> &ans
+        if(open_avail==0 && close_avai==0) ans.push_back(temp);
+        if(open_avail>0) createCombos( open_avail-1 , close_avai , temp+"(", ans);
+        if(close_avai>0 && open_avail<close_avai) createCombos( open_avail , close_avai-1 , temp+")", ans);
+
+        //open_avail<close_avai (OR) opened_in_string > closed_in_string, only then will we be able to close
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        createCombos(n,n,"",ans);
+        return ans;
+    }
+};

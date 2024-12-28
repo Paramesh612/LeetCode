6295783cc9f5777fa990571c00a7f7f7afdf7e93
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-12-29 00:16:45 +0530

    Time: 0 ms (100%), Space: 13.1 MB (78.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -21,16 +21,27 @@ public:
     // See first 5 mins or so of NeetCode's vid
 
     // Maximum that can be opened or closed = n; so 
-    void createCombos( int open_avail , int close_avai, string temp , vector<string> &ans){ //,vector<string> &ans
-        if(open_avail==0 && close_avai==0) ans.push_back(temp);
-        if(open_avail>0) createCombos( open_avail-1 , close_avai , temp+"(", ans);
-        if(close_avai>0 && open_avail<close_avai) createCombos( open_avail , close_avai-1 , temp+")", ans);
+    void createCombos( int open_avail , int close_avail, string &temp , vector<string> &ans){ //,vector<string> &ans
+        if(open_avail==0 && close_avail==0) ans.push_back(temp);
 
-        //open_avail<close_avai (OR) opened_in_string > closed_in_string, only then will we be able to close
+        
+        if(open_avail>0){ 
+            temp+="(";
+            createCombos( open_avail-1 , close_avail , temp , ans);
+            temp.pop_back();
+        }
+
+        if(close_avail>0 && open_avail<close_avail) {
+            temp+=")";
+            createCombos( open_avail , close_avail-1 , temp, ans);
+            temp.pop_back();
+        }
+        //open_avail<close_avail (OR) opened_in_string > closed_in_string, only then will we be able to close
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        createCombos(n,n,"",ans);
+        string temp="";
+        createCombos(n,n,temp,ans);
         return ans;
     }
 };

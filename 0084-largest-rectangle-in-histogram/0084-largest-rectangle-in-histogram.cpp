23eaaab9b3758a18f0c5b51992c16c6c159a8f37
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-12-30 01:00:07 +0530

    Time: 66 ms (8.63%), Space: 86.4 MB (24.37%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -9,7 +9,7 @@ private:
 public:
     // Saw the hint of taking NextSmall and PrevSmall. Others I cracked :)
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
+        int n=heights.size(), ans = 0;
         stack<pair<int,int>> mono , mono2; // Value , index
         vector<int> nextSmall(n), prevSmall(n);
 
@@ -17,30 +17,19 @@ public:
         mono2.push({-1,-1});  // Helps to save writing some 'if' cases 
 
         for(int i=n-1 ; i>=0; i--){
+            // Finding NextSmallest
             while(mono.top().first >= heights[i]) mono.pop();
             nextSmall[i]=mono.top().second;
             if(nextSmall[i] == -1) nextSmall[i] = n;
             mono.push({heights[i],i});
 
+            //Finding PrevSmallest
             int j = n-1-i;
             while(mono2.top().first >= heights[j]) mono2.pop();
             prevSmall[j]=mono2.top().second;
             mono2.push({heights[j],j});
         }
-
-        while(mono.top()!=make_pair(-1,-1)) mono.pop();  // Emptying stack
-
-        // for(int j=0; j<n ; j++){
-        // }
-
-        cout<<"prevSmall: "; print(prevSmall);
-        cout<<"nextSmall: "; print(nextSmall);
-
-        int ans = 0;
-        for(int i=0 ; i<n; i++){
-            ans = max(ans , ((nextSmall[i]-1)-(prevSmall[i]+1)+1)*heights[i] );
-        }
-
+        for(int i=0 ; i<n; i++) ans = max(ans , ((nextSmall[i]-1)-(prevSmall[i]+1)+1)*heights[i] );
         return ans;
     }
 };

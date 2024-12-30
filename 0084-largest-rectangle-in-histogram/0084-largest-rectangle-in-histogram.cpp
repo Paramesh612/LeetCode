e3cdea84c0e150b287927b28ed49f12ab384e81b
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2024-12-30 14:24:36 +0530

    Time: 15 ms (91.35%), Space: 79.6 MB (84.07%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,18 @@
-
 class Solution {
-private:
-    template <typename T>
-    void print(vector<T> v){
-        for(auto &x:v) cout<<x<<"  ";
-        cout<<endl;
-    }
 public:
+    // SEE submission at 2:21 ish for explanation with comments 
     // Saw the hint of taking NextSmall and PrevSmall. Others I cracked :)
     int largestRectangleArea(vector<int>& heights) {
-        //STOP STALKING ME BLUD
-        
         int n=heights.size(), ans = 0;
-        stack<pair<int,int>> mono; // Value , index
-        // vector<int> prevSmall(n); // nextSmall(n),
-
-        pair<int,int> stopper = {-1,-1};
-        mono.push(stopper);  // Helps to save writing some 'if' cases 
-
-        for(int j=0 ; j<n; j++){
-            //Finding PrevSmallest
-            while(mono.top().first >= heights[j]){ 
-                /*
-                ans = max( ans, ((j-1)-(prevSmall[mono.top().second]+1)+1)*heights[mono.top().second] );
-                Simplifying the formula:  */
-
-                // What is the prevSmall value. Isnt it actually the element below????
-                // Then why store it
-
-                int indexOfOldTop = mono.top().second;
+        stack<int> mono; 
+        mono.push(-1);  
+        for(int j=0 ; j<=n; j++){
+            while(mono.top()!=-1 && (j==n || heights[mono.top()] >= heights[j])){ 
+                int indexOfOldTop = mono.top();
                 mono.pop();
-
-                // The new top is the nextSmall of that old top
-                ans = max( ans, (j-mono.top().second-1)*heights[indexOfOldTop]);
-
-
+                ans = max( ans, (j-mono.top()-1)*heights[indexOfOldTop]);
             }
-            mono.push({heights[j],j});
-
-        }
-        
-        while(mono.top() != stopper){ 
-            int indexOfOldTop = mono.top().second;
-            mono.pop();
-            ans = max( ans, (n-mono.top().second-1)*heights[indexOfOldTop]);
+            mono.push(j);
         }
         return ans;
     }

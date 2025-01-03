e91535b542ccefd11b12df31e7fadbd3209b6f6a
Parameshwaran K <136327127+Paramesh612@users.noreply.github.com>
2025-01-03 23:29:25 +0530

    Time: 0 ms (100%), Space: 29 MB (34.97%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -15,7 +15,7 @@ public:
         if(!root1 && !root2) return true;
         if(!root1 || !root2) return false;
 
-        if(root1->val!=root2->val) return false;
+        if(root1->val != root2->val) return false;
         return (isEqual(root1->left,root2->left) && isEqual(root1->right,root2->right));
     }
 
@@ -25,6 +25,9 @@ public:
             if(isEqual(root,subRoot)) return true;
         }
         return (isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot));
-
     }
+
+
+
+    
 };
